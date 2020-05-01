de5e0c8967e53b143b38a6a620620a4671169fe5
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-05-01 15:32:28 +0100

    morgan loggin + error handling

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+/* IMPORTS */ 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
 
+app.use(morgan('dev')) // before routes
 
 /* ROUTES */ 
 const orderRoutes = require('./api/routes/orders')
 const productRoutes = require('./api/routes/products')
-
-/* MIDDLEWARE */ 
+ 
+/* MIDDLEWARE */   
 app.use('/products', productRoutes); // all '/products' requst will be forwarded to the productRoutes
-
 app.use('/orders', orderRoutes);
 
+app.use((req,res,next) => { // handle error for unknown routes, to send specific message
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error)
+})
+
+app.use((error,req,res,next) => { // to handle all errors incliding DB ones
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message 
+        }
+    })
+});
 
 module.exports = app;
 
